607183224ecd7b5d994deb4da9738fcede3c3673
Pranav Prakash <pranavprakash2023@gmail.com>
2024-10-24 20:39:34 +0530

    Movies Redux Slice created

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header';
+import { useDispatch } from 'react-redux';
+import {addNowPlayingMovies} from '../utils/moviesSlice';
 
 const Browse = () => {
+
+  const dispatch  = useDispatch();
+
+  const url = 'https://newly-released-movies.p.rapidapi.com/movies';
+  const options = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+      'x-rapidapi-host': 'newly-released-movies.p.rapidapi.com'
+    }
+  };
+
+  const nowPlayingMovies = async() =>{
+      const response = await fetch(url, options);
+      const result = await response.json();
+      console.log(result);
+      dispatch(addNowPlayingMovies(result));
+  }
+
+  useEffect(()=>{
+    nowPlayingMovies();
+  }, [])
+
+
   return (
     <Header />
   )
